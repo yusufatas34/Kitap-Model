458f67be2acf417739532b640a83055c4421518e
yusufatas34 <atasy674@gmail.com>
2024-04-28 01:07:17 +0300

    Yusufun Kitap Modeli

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Yusufun_Kitap_Modeli.Models;
+
+namespace Yusufun_Kitap_Modeli.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/Kitap.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Yusufun_Kitap_Modeli.Models
+{
+    public class Kitap
+    {
+        public string? KitapAd;
+        public string? YazarAd;
+        public int YayinlanmaTarihi;
+        public string? KitapTuru;
+        public int SayfaSayisi;
+       public string? Fiyat;
+    }
+    }
